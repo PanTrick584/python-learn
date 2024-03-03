ece565bbd11f124cde8a02a059a0168d57d04039
Patryk Chodacki <chodacki.pc@gmail.com>
2024-03-03 10:55:36 +0100

    initial commit

---
+++ index-1.py
@@ -0,0 +1,89 @@
+# print("Hello World!")
+# input("What is your name?")
+# print("Hello " + input("What is your name?") + "!")
+
+# print(len("siemma mmiszczu"))
+
+# name = input("What is your name?")
+# length = len(name)
+# print(name)
+# print(length)
+
+# STRING
+# print("hello"[2])
+# la_la = input()
+# print(type(la_la))
+
+# # NUMBERS
+# 123_345_567
+# # FLOATS
+# 3.234234
+
+# # BOOLEAN
+# True
+# False
+
+# BMI CALCULATOR
+# print("Weight: ")
+# weight = int(input())
+# print("height(meters): ")
+# height = float(input())
+# print(f"You're weight is {weight} and height is {height} and you BMI is {weight / (height * height)}")
+
+# WEEKS OF LIFE
+# age = int(input("How old are you?"))
+# life_left = (90 * 52) - (age * 52)
+# print(f"You have left {life_left} weeks")
+
+# TIP CALCULATOR
+# total_bill = int(input("how many did you pay? : "))
+# total_tip = int(input(f"How big tip in % do you want to live here? : "))
+# total_people = int(input("How many people will pay? : "))
+# indyvidual_pay = (total_bill + (total_bill * (total_tip / 100))) / total_people
+# print(f"Every each of you will pay {indyvidual_pay}")
+
+# BMI CALCULATOR 2.0
+# print("Weight: ")
+# weight = int(input())
+# print("height(meters): ")
+# height = float(input())
+# bmi = weight / (height * height)
+# print(f"You're weight is {weight} and height is {height} and you BMI is {bmi}")
+# if bmi < 18.5:
+#     print(f"Your BMI is {bmi}, you are underweight")
+# elif bmi > 18.5 and bmi < 25:
+#      print(f"Your BMI is {bmi}, you are completley fine!")
+# elif bmi > 25 and bmi < 30:
+#      print(f"Your BMI is {bmi}, you are slightly overweight")
+# elif bmi > 30 and bmi < 35:
+#      print(f"Your BMI is {bmi}, you are obese :/")
+# elif bmi > 35 :
+#      print(f"Your BMI is {bmi}, you are fat pig!")
+
+# PYTHON PIZZA
+# pizza_bill = 0
+# size = input("What size of pizza do you want? (L, M, S) : ")
+# extra_pepperoni = input("Do you want extra pepperoni? Y or N : ")
+# extra_cheese = input("Do you want extra cheese? Y or N : ")
+
+# if size == 'L':
+#     pizza_bill += 20
+# elif size == 'M':
+#     pizza_bill += 17
+# elif size == 'S':
+#     pizza_bill += 14
+# if extra_pepperoni == 'Y':
+#      pizza_bill += 4
+# if extra_cheese == 'Y':
+#      pizza_bill += 6
+
+# thank_you =f"Ok then! Your pizza cost is ${pizza_bill}. Thank ou for you order!"
+# print(thank_you)
+
+# DINNER ROULETTE
+# import random
+# names_list = ["Małgosia", "Patryk", "Małgosia", "Małgosia"]
+# rand_num = random.randint(0, len(names_list) - 1)
+# print(f"Today will pay {names_list[rand_num]}")
+
+# RANGE
